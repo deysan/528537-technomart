3172c5625a2d86b895c44ccfe546f5d4f9d75042
deysan <ideysan@gmail.com>
2018-09-24 03:08:34 +0300

    Обновил скрипт

--- js/script.js
+++ js/script.js
@@ -6,18 +6,32 @@ var map = document.querySelector(".contacts__map");
 var maps = document.querySelector(".modal-map");
 var overlay = document.querySelector(".modal-overlay");
 var close = document.querySelectorAll(".modal-close");
+var formfeed = feedback.querySelector(".modal-feedback__form");
+var fullname = feedback.querySelector("[name=fullname]");
+var email = feedback.querySelector("[name=email]");
+var text = feedback.querySelector("[name=text]");
+var isStorageSupport = true;
+var storage_fullname = "";
+var storage_email = "";
+
+try {
+  storage_fullname = localStorage.getItem("fullname");
+  storage_email = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 if (tobuy,cart) {
   for (var i = 0; i < tobuy.length; i++) {
-    tobuy[i].addEventListener("click", function (evt) {
-      evt.preventDefault();
+    tobuy[i].addEventListener("click", function (event) {
+      event.preventDefault();
       cart.classList.add("modal-show");
       overlay.classList.add("modal-show");
     });
   };
   for (var i = 0; i < close.length; i++) {
-    close[i].addEventListener("click", function (evt) {
-      evt.preventDefault();
+    close[i].addEventListener("click", function (event) {
+      event.preventDefault();
       cart.classList.remove("modal-show");
       overlay.classList.remove("modal-show");
     });
@@ -30,34 +44,57 @@ if (tobuy,cart) {
 }
 
 if (form,feedback) {
-  form.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  form.addEventListener("click", function (event) {
+    event.preventDefault();
     feedback.classList.add("modal-show");
     overlay.classList.add("modal-show");
+    if (storage_fullname) {
+      fullname.value = storage_fullname;
+      email.focus();
+    } if (storage_email) {
+      email.value = storage_email;
+      text.focus();
+    } else {
+      fullname.focus();
+    }
   });
   for (var i = 0; i < close.length; i++) {
-    close[i].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      feedback.classList.remove("modal-show");
+    close[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      feedback.classList.remove("modal-show","modal-error");
       overlay.classList.remove("modal-show");
     });
   };
   overlay.addEventListener("click", function(event) {
     event.preventDefault();
-    feedback.classList.remove("modal-show");
+    feedback.classList.remove("modal-show","modal-error");
     overlay.classList.remove("modal-show");
+  });
+  formfeed.addEventListener("submit", function (event) {
+    if (!fullname.value || !email.value || !text.value) {
+      event.preventDefault();
+      feedback.classList.remove("modal-error");
+      feedback.offsetWidth = feedback.offsetWidth;
+      feedback.classList.add("modal-error");
+      console.log("Нужно заполнить все поля");
+    } else {
+      if (isStorageSupport) {
+      localStorage.setItem("fullname", fullname.value);
+      localStorage.setItem("email", email.value);
+      }
+    }
   });
 }
 
 if (map,maps) {
-  map.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  map.addEventListener("click", function (event) {
+    event.preventDefault();
     maps.classList.add("modal-show");
     overlay.classList.add("modal-show");
   });
   for (var i = 0; i < close.length; i++) {
-    close[i].addEventListener("click", function (evt) {
-      evt.preventDefault();
+    close[i].addEventListener("click", function (event) {
+      event.preventDefault();
       maps.classList.remove("modal-show");
       overlay.classList.remove("modal-show");
     });
@@ -76,7 +113,7 @@ window.addEventListener("keydown", function(event) {
       overlay.classList.remove("modal-show");
     }
     if (feedback.classList.contains("modal-show") && overlay.classList.contains("modal-show")) {
-      feedback.classList.remove("modal-show");
+      feedback.classList.remove("modal-show","modal-error");
       overlay.classList.remove("modal-show");
     }
     if (maps.classList.contains("modal-show") && overlay.classList.contains("modal-show")) {
